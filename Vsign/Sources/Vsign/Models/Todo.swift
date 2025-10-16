75026024a4757988cbeb0e25a86c0b6b08f89253
Agatha Schneider <agatha.schneider@outlook.com>
2025-10-16 15:49:36 -0300

    modifying the model to add a field
    After changes we should:
    
    // Drop the DB
    1) DROP DATABASE vapor;
    
    // Delete the remainings
    2) DELETE FROM _fluent_migrations WHERE name = 'CreateTodo';
    
    // Create the DB
    3) CREATE DATABASE vapor;
    
    // Run the migration again
    4) swift run Vsign migrate

--- Vsign/Sources/Vsign/Models/Todo.swift
+++ Vsign/Sources/Vsign/Models/Todo.swift
@@ -1,4 +1,5 @@
 import Fluent
+
 import struct Foundation.UUID
 
 /// Property wrappers interact poorly with `Sendable` checking, causing a warning for the `@ID` property
@@ -6,24 +7,29 @@ import struct Foundation.UUID
 /// afterwards with `@unchecked Sendable`.
 final class Todo: Model, @unchecked Sendable {
     static let schema = "todos"
-    
+
     @ID(key: .id)
     var id: UUID?
 
     @Field(key: "title")
     var title: String
 
-    init() { }
+    @Field(key: "completed")
+    var completed: Bool
 
-    init(id: UUID? = nil, title: String) {
+    init() {}
+
+    init(id: UUID? = nil, title: String, completed: Bool) {
         self.id = id
         self.title = title
+        self.completed = completed
     }
-    
+
     func toDTO() -> TodoDTO {
         .init(
             id: self.id,
-            title: self.$title.value
+            title: self.$title.value,
+            completed: self.$completed.value
         )
     }
 }
